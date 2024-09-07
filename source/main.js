81d1af7b2993c3f14ed8019cb47ea45b1c47b030
Eaxon <aeurum@gmail.com>
2024-09-07 21:23:21 +0300

    Added `symbol` and `link`

--- source/main.js
+++ source/main.js
@@ -3,9 +3,12 @@ import { mdv2 } from './mdv2'
 import { html } from './html'
 import { mdv1 } from './mdv1'
 import { newlines } from './newlines'
+import { symbol } from './symbol'
+import { link } from './link'
 
 export {
   STYLES, STYLES as PARSE_MODES,
   mdv2, mdv2 as md, html, mdv1,
-  newlines, newlines as ln
+  newlines, newlines as ln,
+  symbol, symbol as sm, link, link as telink
 }
